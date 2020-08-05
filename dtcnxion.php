94d2fc17a798f6bbe96fbce5f8a9777fb3c29c16
Habibie <habibieamrullah@gmail.com>
2020-08-05 15:50:22 +0700

    Update dtcnxion.php

--- dtcnxion.php
+++ dtcnxion.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost", "rumf5183_habibie", "sinchan*123#", "rumf5183_ciihuy");
+$connection = mysqli_connect("localhost", "dbuser", "dbpassword", "dbtable");
 //$connection->set_charset("utf8");
 mysqli_set_charset($connection,'utf8');
 ?>
